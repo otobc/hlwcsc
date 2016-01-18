1a648c86b99d5d916dc6f655e896f61f3aff29f6
mike <1144549013@qq.com>
2016-01-18 10:37:11 +0800

    init,range,insert,search

--- ROOT/WebRoot/configfile/evaluation_config.sql
+++ ROOT/WebRoot/configfile/evaluation_config.sql
@@ -1,27 +1,27 @@
 create database evaluation_config;
 use evaluation_config;
-#drop table if exists wpindex;
-#drop table if exists vertex;
-#drop table if exists edge;
-#drop table if exists evaluate;
-#drop table if exists rawdata;
-#drop table if exists wpidxcache;
-#drop table if exists experiment;
+drop table if exists wpindex;
+drop table if exists vertex;
+drop table if exists edge;
+drop table if exists evaluate;
+drop table if exists rawdata;
+drop table if exists wpidxcache;
+drop table if exists experiment;
 
 create table wpindex(
-    id		char(8)		not null,
+    id		varchar(64)		not null,
     name	varchar(256) 	not null,
     expression  varchar(1024)   not null,
     primary key (id)
 );
 
 create table vertex (
-    evaluateid  char(8)         not null,
-    id		char(8)		not null,
+    evaluateid  varchar(64)         not null,
+    id		varchar(64)		not null,
     name	varchar(256)	not null,
     class	char(1)		not null,
     vertextype  char(1)		not null,
-    wpindexid char(8),
+    wpindexid varchar(64),
     nmidx	char(2),
     nmidxexp	varchar(1024),
     vertexlevel varchar(64),
@@ -29,30 +29,32 @@ create table vertex (
     nmlvlexp	varchar(1024),
     mbsp	varchar(1024),
     conclude	char(2),
+    amnwt	double(9,8),
+    mnwting char(2),
     primary key (evaluateid,id)
 );
 
 create table edge (
-    evaluateid	char(8)		not null,
-    child	char(8)		not null,
-    parent	char(8)		not null,
-    weight	double(8,8)	not null,
+    evaluateid	varchar(64)		not null,
+    child	varchar(64)		not null,
+    parent	varchar(64)		not null,
+    weight	double(9,8)	not null,
     weighting	char(2),
     primary key (evaluateid,child,parent)
 );
 
 create table evaluate (
-    id		char(8)		not null,
+    id		varchar(64)		not null,
     name	varchar(256)	not null,
-    expid	char(8)		not null,
+    expid	varchar(64)		not null,
     primary key (id)
 );
 
 create table rawdata (
-    id		char(8)		not null,
+    id		varchar(64)		not null,
     name	char(8)		not null,
-    environmentid char(8)	not null,
-    nodeid	char(8)		not null,
+    environmentid varchar(64)	not null,
+    nodeid	varchar(64)		not null,
     datatype	char(1)		not null,
     value	varchar(256),
     time	char(22)	not null,
@@ -60,16 +62,16 @@ create table rawdata (
 );
 
 create table wpidxcache (
-    evaluateid	char(8)		not null,
-    wpindexid	char(8)		not null,
-    experimentid char(8)	not null,
+    evaluateid	varchar(64)		not null,
+    wpindexid	varchar(64)		not null,
+    experimentid varchar(64)	not null,
     datatype	char(1)		not null,
     value	varchar(256),
     primary key (evaluateid,wpindexid,experimentid)
 );
 
 create table experiment (
-    id		char(8)		not null,
+    id		varchar(64)		not null,
     name 	varchar(256)	not null,
     wptype	char(1)		not null,
     tester	varchar(256)	not null,
---
+++ ROOT/src/bean_json/BeanToJson.java
@@ -0,0 +1,114 @@
+package bean_json;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class BeanToJson
+{
+	public String getInitJson(String table)
+	{
+		String path = null;
+		try
+		{
+			path = JsonToBean.class.getClassLoader()
+					.getResource("../../configfile/init" + table + ".json")
+					.toURI().getPath();
+		} catch (URISyntaxException e1)
+		{
+			e1.printStackTrace();
+		}
+		File initFile = new File(path);
+		String initJson = "";
+		Scanner input = new Scanner("");
+		try
+		{
+			input = new Scanner(initFile);
+			while (input.hasNextLine())
+			{
+				initJson = initJson + input.nextLine();
+			}
+		} catch (FileNotFoundException e)
+		{
+			// 响应失败json
+			e.printStackTrace();
+		} finally
+		{
+			input.close();
+		}
+		return initJson;
+	}
+
+	public String getRangeJson(ResultSet resultSet) throws IOException,
+			JsonGenerationException, JsonMappingException
+	{
+		String rangeJson="";
+		// 对数据库执行结果处理，为bean赋值,转化为json串
+		ArrayList<KVBean> kvBeanList = new ArrayList<KVBean>();
+		try
+		{
+			while (resultSet.next())
+			{
+				KVBean kvBean = new KVBean();
+				kvBean.key = resultSet.getString(1);
+				kvBean.value = resultSet.getString(2);
+				kvBeanList.add(kvBean);
+			}
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+
+		RangeBean bean = new RangeBean();
+		bean.result = "00";
+		bean.message = "OK";
+		bean.data = kvBeanList;
+		ObjectMapper mapper = new ObjectMapper();
+		rangeJson = mapper.writeValueAsString(bean);
+
+		return rangeJson;
+	}
+
+	public String getInsertJson(int result)
+	{
+		String insertJson = "";
+		if (result == 1)
+		{
+			insertJson = "{\"result\":\"00\",\"message\":\"OK\"}";
+			System.out.println("insert successfully,result=" + result);
+		}
+		else
+		{
+			insertJson = "{\"result\":\"01\",\"message\":\"OK\"}";
+			System.out.println("insert failed,result=" + result);
+		}
+		return insertJson;
+	}
+
+	public String getSearchJson(ResultSet resultSet, String begin, String count)
+	{
+		String searchJson="";
+		try
+		{
+			while(resultSet.next())
+			{
+				//x,y,post/get
+				//处理执行结果赋值给bean，bean to json
+				//重构URLtoSQLtoResult类，sql产生和执行分开
+			}
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return searchJson;
+	}
+}
